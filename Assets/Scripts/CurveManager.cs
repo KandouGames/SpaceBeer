1df168db047b54a080eb8be621357fff4d1db2ad
Carlos Rizo <carima96@gmail.com>
2020-02-19 20:31:16 +0100

    Ported to 2018.4

---
+++ Assets/Scripts/CurveManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class CurveManager : MonoBehaviour
+{
+
+    public float curveRadius;
+    public float pipeRadius;
+    public int curveSegmentCount;
+    public int pipeSegmentCount;
+
+    public void GetPointTorus()
+    {
+
+    }
+}
