42a14b3784b4ffe7eaa8ff69a507a4952f85e6c5
Manuel19git <manuelshino@gmail.com>
2020-02-26 18:49:09 +0100

    Metido la escena de manuel y sus recursos

---
+++ Assets/Scripts/Combustible.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combustible : MonoBehaviour
+{
+    float FULL_FUEL = -90.0f;
+    float ZERO_FUEL = 90.0f;
+    private Transform needleTransform;
+
+    private float fuelMax;
+    private float fuel;
+
+    private void Awake()
+    {
+        needleTransform = transform.Find("needle");
+
+        fuel = 200f;
+        fuelMax = 200f;
+    }
+
+    private void Update()
+    {
+        fuel -= 10f * Time.deltaTime;
+
+        if (fuel < 0) fuel = 0;
+
+        needleTransform.localEulerAngles = new Vector3(0, 0, GetFuelRotation());
+    }
+
+    private float GetFuelRotation()
+    {
+        float totalAngle = Mathf.Abs(FULL_FUEL - ZERO_FUEL);
+
+        //Valores del combustible entre 0 y 1
+        float fuelNormalized = (fuelMax - fuel) / fuelMax;
+
+        return FULL_FUEL + fuelNormalized * totalAngle;
+    }
+
+}
