eb8661f90377d3fac479e3c0e57a1192dc50925f
Manuel19git <manuelshino@gmail.com>
2020-05-07 23:40:59 +0200

    Added camera control

---
+++ Assets/Resources/MenusManuel/CameraController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform[] views;
+    public float transitionSpeed;
+    Transform currentView;
+    Button backHangar;  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Default
+        currentView = views[0];
+    }
+
+    void Update()
+    {
+
+    }
+
+    //LateUpdate para interpolaciones lineales
+    void LateUpdate()
+    {
+        
+        //Lerp position
+        transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * transitionSpeed);
+    }
+
+    void SetCameraStartMenu()
+    {
+        currentView = views[0];
+    }
+
+    void SetCameraHangarMenu()
+    {
+        currentView = views[1];
+    }
+
+
+}
