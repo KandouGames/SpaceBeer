ab25528f0098e6a094aa8f2658edd84501b022d5
Joan Torres <sindoparme@gmail.com>
2020-05-10 23:08:30 +0200

    Finished Scoring Manager + Gameplay UI

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    private const int maxBarrels = 4;
+    private const int iniBarrels = 2;
+
+    private int beerCoins;
+    private ulong distance;
+    private int barrels;
+    public UIManager uiManager;
+
+    void Start()
+    {
+        startNewGame();
+    }
+
+    void Update()
+    {
+        distance += 1;
+        uiManager.setDistance(distance);
+
+        // Para probar
+        if (Input.GetKeyDown(KeyCode.Z))
+            earnBarrel();
+
+        if (Input.GetKeyDown(KeyCode.X))
+            looseBarrel();
+
+        if (Input.GetKeyDown(KeyCode.C))
+            distributeBarrel();
+    }
+
+    public void startNewGame()
+    {
+        beerCoins = 0;
+        distance = 0;
+        barrels = iniBarrels;
+
+        uiManager.setBeerCoins(beerCoins);
+        uiManager.setDistance(distance);
+        uiManager.setBarrels(barrels);
+    }
+
+    public void earnBarrel()
+    {
+        
+        if (barrels < maxBarrels)
+        {
+            ++barrels;
+            uiManager.setBarrels(barrels);
+        }
+
+
+    }
+
+    public void distributeBarrel()
+    {
+        switch (barrels)
+        {
+            case 0:
+                break;
+
+            case 1:
+                beerCoins += 225;
+                --barrels;
+                break;
+
+            case 2:
+                beerCoins += 200;
+                --barrels;
+                break;
+
+            case 3:
+                beerCoins += 125;
+                --barrels;
+                break;
+
+            case 4:
+                beerCoins += 100;
+                --barrels;
+                break;
+        }
+
+        
+        uiManager.setBarrels(barrels);
+        uiManager.setBeerCoins(beerCoins);
+
+    }
+
+    public void looseBarrel()
+    {
+        if (barrels == 0)
+        {
+            uiManager.showGameOver(beerCoins, distance);
+            print("game over");
+        } else
+        {
+            --barrels;
+            uiManager.setBarrels(barrels);
+        }
+            
+            
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text beerCoinsText;
+    public Text distanceText;
+    public List<GameObject> arrayBarrels;
+    public GameObject gameOverUI;
+    public Text gameOverRecordText;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void setBeerCoins(int beerCoins)
+    {
+        beerCoinsText.text = beerCoins.ToString();
+    }
+
+    public void setDistance(ulong distance)
+    {
+        distanceText.text = distance.ToString();
+    }
+
+    public void setBarrels(int barrels)
+    {
+        for(int i = 0; i < arrayBarrels.Count; ++i)
+        {
+            if (i < barrels)
+                arrayBarrels[i].SetActive(true);
+            else
+                arrayBarrels[i].SetActive(false);
+        }
+
+    }
+
+    public void showGameOver(int beerCoins, ulong distance)
+    {
+        gameOverUI.SetActive(true);
+        gameOverRecordText.text = "You won " + beerCoins.ToString() + " beercoins \n and have traveled " + distance.ToString() + " km";
+    }
+}
