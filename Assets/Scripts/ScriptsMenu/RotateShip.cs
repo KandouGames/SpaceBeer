ad5f5d9f07461e4248a451b99e13a13a6bed7422
Cristian <cristianpenizzotto@yahoo.com.ar>
2020-05-11 18:34:02 +0200

    Actualizacion de menu para integracion
    Actualizacion para integracion de menu

--- Assets/Scripts/ScriptsMenu/RotateShip.cs
+++ Assets/Scripts/ScriptsMenu/RotateShip.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotateScenary : MonoBehaviour
+public class RotateShips : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
