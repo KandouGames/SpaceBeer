2b4de37a7f3c24d03ed664767f46b5f1bd610423
Manuel19git <manuelshino@gmail.com>
2020-05-29 20:36:37 +0200

    Select SpaceShips from menu and added to gameplay

---
+++ Assets/Scripts/Managers/PlayerData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public List<GameObject> spaceShips;
+    public List<GameObject> weapons;
+    public int spaceShipID;
+    public int weaponID;
+
+    
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
