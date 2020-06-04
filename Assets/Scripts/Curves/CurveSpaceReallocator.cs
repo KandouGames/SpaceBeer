f5daaccaff3e879c13db76cb8875a2c0c69b902a
Carlos Rizo <carima96@gmail.com>
2020-06-04 17:40:08 +0200

    Changed Curve data to make it smoother. Solved space atrezzo movement with a hack

---
+++ Assets/Scripts/Curves/CurveSpaceReallocator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// When moving the space bubble with the curve we change its position a bit at a time
+/// To prevent the space bubble to move and be left behind by the curve we reallocate it 
+/// frame by frame deleting the delta change of position the curve needs. Not elegant but it works
+/// </summary>
+public class CurveSpaceReallocator : MonoBehaviour
+{
+    void Update()
+    {
+        gameObject.transform.position = Vector3.zero;
+    }
+}
